3425992795b2034ee72ee2356932ffb95013f9ce
vishwae-milieu <vishwa.e@milieuglobal.com>
2026-08-12 00:06:49 +0530

    updated backend url in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const API_BASE_URL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://project1-backend-j27g.onrender.com/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
